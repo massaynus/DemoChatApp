d434217c9415177938dce7af3cb2dbf79686f830
massaynus <massaoudi.yass@gmail.com>
2022-03-06 00:31:39 +0100

    EDIT (chatAPI): HTTPs redirect in Prod only

--- server/chatAPI/Program.cs
+++ server/chatAPI/Program.cs
@@ -16,8 +16,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseHttpsRedirection();
+}
 
-// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
