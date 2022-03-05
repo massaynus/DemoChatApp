5dedcec4a6e98d436ffe2c53bdd26bf036bc08cc
massaynus <massaoudi.yass@gmail.com>
2022-03-05 23:33:07 +0100

    changed react app to TS template

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -1,24 +1,15 @@
-import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
-const rootElement = document.getElementById('root');
-
 ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
-  rootElement);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
