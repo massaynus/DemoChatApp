ebca7dc9891f70cacf22a7714d9b29a5127d4556
massaynus <massaoudi.yass@gmail.com>
2022-03-09 02:13:48 +0100

    maybe !

--- server/chatAPI/DTOs/SignUp/UserSignUpRequest.cs
+++ server/chatAPI/DTOs/SignUp/UserSignUpRequest.cs
@@ -4,17 +4,17 @@ namespace chatAPI.DTOs;
 
 public class UserSignUpRequest
 {
-    public string FirstName { get; set; }
+    public string FirstName { get; set; } = string.Empty;
 
-    public string LastName { get; set; }
+    public string LastName { get; set; } = string.Empty;
 
     [Required]
     public string Username { get; set; }
 
-    public string Email { get; set; }
+    public string Email { get; set; } = string.Empty;
 
     [Required]
     public string Password { get; set; }
 
-    public DateTime DateOfBirht { get; set; }
+    public DateTime DateOfBirht { get; set; } = DateTime.UtcNow;
 }
